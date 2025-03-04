70c6a17e6ce6f538d4e67f704a6c6341265cec6e
unknown <madacostac2@gmail.com>
2025-03-04 22:49:12 +0100

    chore: update eslint and gitignore

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,63 @@
+const { fs } = require("fs");
+
+const srcFolders = fs
+  .readdirSync("./src", { withFileTypes: true })
+  .filter((dirent) => dirent.isDirectory())
+  .map((dirent) => dirent.name);
+
 module.exports = {
   parserOptions: {
     tsconfigRootDir: __dirname,
+  },
+  settings: {
+    "import/resolver": {
+      alias: {
+        map: [srcFolders.map((folder) => [folder, `./src/${folder}`])],
+        extensions: [".ts", ".tsx"],
+      },
+    },
+  },
+  rules: {
+    "import/order": [
+      "error",
+      {
+        pathGroups: [
+          {
+            pattern: `+(${srcFolders.join("|")})/**`,
+            group: "parent",
+            position: "before",
+          },
+        ],
+        pathGroupsExcludedImportTypes: ["builtin"],
+        "newlines-between": "never",
+      },
+    ],
+
+    // ban some imports
+    "no-restricted-imports": [
+      "error",
+      {
+        patterns: [
+          {
+            group: ["**/dtos", "!common/dtos"],
+            message: "Please keep dtos into common/dtos folder.",
+          },
+          {
+            group: [
+              "**/utils/*",
+              "**/shared/*",
+              "**/constants/*",
+              "common/dtos/*",
+              "**/data/*",
+              "**/types/*",
+              "**/hocs/*",
+              "**/hooks/*",
+              "!**/hooks/*.d",
+            ],
+            message: "Please import directly from the index.",
+          },
+        ],
+      },
+    ],
   },
 };
